2c2c4d53ec86967c7fa85dc1a5b0dc03bc43247a
ibiann <trungpham152000@gmail.com>
2022-06-09 17:08:34 +0700

    update

--- kanban/src/util/const.jsx
+++ kanban/src/util/const.jsx
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-export const API_ROOT = "http://localhost:8080";
-=======
 export const API_ROOT = 'http://localhost:8080'
->>>>>>> c60901910162d9149ca4383797481b5458dcf7cf
 
 export const MODAL_CLOSE = "MODAL_CLOSE";
 export const MODAL_CONFIRM = "MODAL_CONFIRM";
